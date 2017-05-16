835e2456405f4b9c6ef676f55c118de99dd8a1bd
biamarou <biamarou@gmail.com>
2017-05-16 09:12:49 -0300

    Fix the constructor

--- WordFinder.java
+++ WordFinder.java
@@ -1,31 +1,48 @@
 import java.lang.StringBuffer;
 import edu.princeton.cs.algs4.SeparateChainingHashST;
 public class WordFinder {
-    private SeparateChainingHashST hashST;
+    private SeparateChainingHashST<String, Val> hashST;
+
+    private class Val {
+	int line, appear;
+
+	    public Val(int l, int a) {
+		line = l;
+		appear = a;
+	    }
+	}
     
     public WordFinder (String[] words) {
-	hashST = new SeparateChainingHashST();
+	hashST = new SeparateChainingHashST<String, Val>();
 	StringBuffer buffer;
+	int line = 1;
 
-	for (int i = 0; i < words.length(); i++)
-	    for (int j = 0; j < words[i].length(); j++) {
+	for (String s : words) {
+	    for (int j = 0; j < s.length(); j++) {
 		buffer = new StringBuffer();
-		char letter = words[i][j];
+		char letter = s.charAt(j);
 
 		while (letter != ' ') {
 		    buffer.append(letter);
 		    j++;
-		    letter = words[i][j];
+		    letter = s.charAt(j);
 		}
 		if (buffer.length() > 0) {
-		    if (!hashST.contains(buffer.toString()))
-		        hashST.put(buffer.toString(), 1);
-		    else
-			hashST.put(buffer.toString(), get(buffer.toString()) + 1);
-			    
+		    if (!hashST.contains(buffer.toString())) {
+			Val v = new Val(line, 1);
+			hashST.put(buffer.toString(), v);
+		    }
+		    else {
+			Val v = hashST.get(buffer.toString());
+			if (v.line != line) {
+			    v.appear += 1;
+			    hashST.put(buffer.toString(), v);
+			}
+		    }  
 		}
 		
 	    }
-		
+	    line++;
+	}
     }
 }
